2cfcb92172c60d25d3379694b0ad02a1943fba4e
qlenlen <1783619611@qq.com>
2025-06-11 18:31:58 +0800

    upstream rekernel

--- drivers/rekernel/rekernel.h
+++ drivers/rekernel/rekernel.h
@@ -8,13 +8,17 @@
 #define KERNEL_6_6
 // #define KERNEL_6_12
 
-#define MIN_USERAPP_UID (10000)
-#define MAX_SYSTEM_UID  (2000)
+#define NETWORK_FILTER
+#define CLEAN_UP_ASYNC_BINDER
+
+#define MIN_USERAPP_UID                 (10000)
+#define MAX_SYSTEM_UID                  (2000)
+#define SYSTEM_APP_UID                  (1000)
 #define RESERVE_ORDER					17
 #define WARN_AHEAD_SPACE				(1 << RESERVE_ORDER)
-#define INTERFACETOKEN_BUFF_SIZE (140)
-#define PARCEL_OFFSET (16) /* sync with the writeInterfaceToken */
-#define LINE_ERROR (-1)
-#define LINE_SUCCESS (0)
+#define INTERFACETOKEN_BUFF_SIZE        (140)
+#define PARCEL_OFFSET                   (16) /* sync with the writeInterfaceToken */
+#define LINE_ERROR                      (-1)
+#define LINE_SUCCESS                    (0)
 
 #endif
